b638c964685c57dc1883930bf4604f116bb45397
Jonathon Pinnow <pinnowj@gmail.com>
2023-12-04 16:36:34 -0500

    day one part one success

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("./input_data/d1p1.txt").unwrap();
+    println!("Grabing first and last numbers from each line to then concat into a number and then some each of them");
+    let mut total: u32 = 0;
+
+    for i in input.as_str().split("\n") {
+        let mut nums = i.chars().filter(|c| c.is_numeric());
+        let Some(first) = nums.next() else { return };
+        let mut result = first.to_string();
+
+        if let Some(n) = nums.last() {
+            result.push(n)
+        } else {
+            result.push(first)
+        }
+        total += result.parse::<u32>().unwrap();
+        println!("{result}")
+    }
+    assert_eq!(total, 54331); // this is the correct answer and is here for reference and refactoring
+    println!("total = {total}")
 }
